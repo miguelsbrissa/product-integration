4136ed595b59d056a3a4fa1cedb95779e5dc5aea
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2022-05-29 18:42:20 -0300

    Corrige link para edição de págin
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/miguelsbrissa/product-integration',
+            'https://github.com/miguelsbrissa/product-integration/tree/master',
         },
         blog: false,
         theme: {
